27ca7777d23176f0df606f9f92c58073adb945d2
Franak007 <franck.pertosa@gmail.com>
2023-09-20 12:07:04 +0200

    calcul du solde fonctionnel avec mise à jour si nouvelle opération

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {BudgetService} from "../budget.service";
 import {BudgetLine} from "../bubget-interface";
 import {BudgetLines} from "../mock-budget-list";
@@ -9,24 +9,26 @@ import {NgForm} from "@angular/forms";
   templateUrl: './budget.component.html',
   styleUrls: ['./budget.component.css']
 })
-export class BudgetComponent implements OnInit {
+export class BudgetComponent {
 
   budgetLinesList: BudgetLine[] = BudgetLines;
 
   constructor(private budget: BudgetService) {
   }
 
-  ngOnInit() {
-    // this.getAllBudgetLines();
-  }
-
-  getAllBudgetLines() {
-    this.budgetLinesList = this.budget.findAllBudgetLines();
-  }
-
   onSubmit(form: NgForm){
     console.log(form.value);
     return this.budget.addToBudgetList(form.value);
-    document.location.href=""
+  }
+
+  accountBalance(): number {
+
+    const entries = this.budgetLinesList.filter(budgetLine => budgetLine.type === 'entrée');
+    const expenses = this.budgetLinesList.filter(budgetLine => budgetLine.type === 'dépense');
+
+    const totalEntries = entries.reduce((acc, transaction) => acc + transaction.amount, 0);
+    const totalExpenses = expenses.reduce((acc, transaction) => acc + transaction.amount, 0);
+
+    return totalEntries - totalExpenses;
   }
 }
