8e5fef29055bccc9b45dd89ef3f8e583bd9bd8b5
Franak007 <franck.pertosa@gmail.com>
2023-09-20 09:36:35 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BudgetComponent } from './budget/budget.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BudgetComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
